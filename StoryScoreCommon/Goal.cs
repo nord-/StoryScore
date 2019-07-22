5d7f8d0fb819ee3cfc5933ab63e9805eda3aaa27
Rickard Nord <rickard.nord@empir.se>
2019-07-22 18:34:11 +0200

    refactor: created common project to hold mqtt shared dto:s

---
+++ StoryScoreCommon/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryScoreCommon
+{
+    public class Goal
+    {
+        public int Score { get; set; }
+        public bool IsHomeTeam { get; set; }
+        public string ScorerName { get; set; }
+        public int ScorerNumber { get; set; }
+        public string ScorerImagePath { get; set; }
+        public string ScorerVideoPath { get; set; }
+    }
+}
