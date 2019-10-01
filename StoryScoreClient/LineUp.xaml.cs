acbef2964b20fb9108d9fb61d51125483d56c9c2
Rickard Nord <rickard.nord@combitech.se>
2019-10-01 17:54:24 +0200

    feat: add/remove players to/from lineup

--- StoryScoreClient/LineUp.xaml.cs
+++ StoryScoreClient/LineUp.xaml.cs
@@ -29,5 +29,63 @@ namespace StoryScore.Client
             InitializeComponent();
             DataContext = ViewModel;
         }
+
+        private void AddHomePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedHomePlayerPool != null)
+            {
+                ViewModel.HomeLineUp.Add(ViewModel.SelectedHomePlayerPool);
+                ViewModel.HomeTeam.Players.Remove(ViewModel.SelectedHomePlayerPool);
+            }
+        }
+
+        private void RemoveHomePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedHomePlayerLineup != null)
+            {
+                ViewModel.HomeTeam.Players.Add(ViewModel.SelectedHomePlayerLineup);
+                ViewModel.HomeLineUp.Remove(ViewModel.SelectedHomePlayerLineup);
+            }
+        }
+
+        private void AddHomePlayer_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            AddHomePlayer_Click(sender, null);
+        }
+
+        private void RemoveHomePlayer_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            RemoveHomePlayer_Click(sender, null);
+        }
+
+        private void AddAwayPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedAwayPlayerPool != null)
+            {
+                ViewModel.AwayLineUp.Add(ViewModel.SelectedAwayPlayerPool);
+                ViewModel.AwayTeam.Players.Remove(ViewModel.SelectedAwayPlayerPool);
+            }
+
+        }
+
+        private void RemoveAwayPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedAwayPlayerLineup != null)
+            {
+                ViewModel.AwayTeam.Players.Add(ViewModel.SelectedAwayPlayerLineup);
+                ViewModel.AwayLineUp.Remove(ViewModel.SelectedAwayPlayerLineup);
+            }
+
+        }
+
+        private void AddAwayPlayer_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            AddAwayPlayer_Click(sender, null);
+        }
+
+        private void RemoveAwayPlayer_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            RemoveAwayPlayer_Click(sender, null);
+        }
     }
 }
--- StoryScoreClient/Model/LineupViewModel.cs
+++ StoryScoreClient/Model/LineupViewModel.cs
@@ -11,13 +11,16 @@ namespace StoryScore.Client.Model
     public class LineupViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<TeamViewModel> Teams { get; set; }
-        public ObservableCollection<PlayerViewModel> HomePlayers { get; set; }
-        public ObservableCollection<PlayerViewModel> HomeLineUp { get; set; }
-        public ObservableCollection<PlayerViewModel> AwayPlayers { get; set; }
-        public ObservableCollection<PlayerViewModel> AwayLineUp { get; set; }
+        public ObservableCollection<PlayerViewModel> HomeLineUp { get; set; } = new ObservableCollection<PlayerViewModel>();
+        public ObservableCollection<PlayerViewModel> AwayLineUp { get; set; } = new ObservableCollection<PlayerViewModel>();
 
         public TeamViewModel HomeTeam { get; set; }
         public TeamViewModel AwayTeam { get; set; }
+
+        public PlayerViewModel SelectedHomePlayerPool { get; set; }
+        public PlayerViewModel SelectedAwayPlayerPool { get; set; }
+        public PlayerViewModel SelectedHomePlayerLineup { get; set; }
+        public PlayerViewModel SelectedAwayPlayerLineup { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
